fee09597710bcd02fa1639d107ccf6955d9ec3d6
Patrick Dunn <patrick.jg.dunn@gmail.com>
2024-11-12 23:14:23 -0500

    Made Testimonials section responsive, installed Bootstrap

--- little-lemon/src/components/Testimonials.js
+++ little-lemon/src/components/Testimonials.js
@@ -5,30 +5,34 @@ function Testimonials() {
         <div className="Testimonials">
             <h2>Testimonials</h2>
             <div className="TestimonialsList">
-                <TestimonialCard
-                    rating="5"
-                    profilePic={"https://fer-uig.glitch.me?uuid=1" }
-                    testimonialName="Sam K."
-                    reviewMsg="Lovin' Bunz"
-                />
-                <TestimonialCard
-                    rating="3.5"
-                    profilePic={"https://fer-uig.glitch.me?uuid=102"}
-                    testimonialName="Porspatan"
-                    reviewMsg="Clean dirt"
-                />
-                <TestimonialCard
-                    rating="4.5"
-                    profilePic={"https://fer-uig.glitch.me?uuid=347"}
-                    testimonialName="Crab Gut"
-                    reviewMsg="Oh, them potatoes sure can"
-                />
-                <TestimonialCard
-                    rating="1"
-                    profilePic={"https://fer-uig.glitch.me?uuid=232"}
-                    testimonialName="Alice"
-                    reviewMsg="The server ate part of my child like wtf"
-                />
+                <div className="TestimonialSection">
+                    <TestimonialCard
+                        rating="5"
+                        profilePic={"https://fer-uig.glitch.me?uuid=1" }
+                        testimonialName="Sam K."
+                        reviewMsg="I want to eat bruschetta until I puke"
+                    />
+                    <TestimonialCard
+                        rating="3.5"
+                        profilePic={"https://fer-uig.glitch.me?uuid=102"}
+                        testimonialName="Porspatan"
+                        reviewMsg="I really enjoyed my bowl of dirt, it was very clean dirt"
+                    />
+                </div>
+                <div className="TestimonialSection">
+                    <TestimonialCard
+                        rating="4.5"
+                        profilePic={"https://fer-uig.glitch.me?uuid=347"}
+                        testimonialName="Crab Gut"
+                        reviewMsg="Them potatoes sure can potate"
+                    />
+                    <TestimonialCard
+                        rating="1"
+                        profilePic={"https://fer-uig.glitch.me?uuid=232"}
+                        testimonialName="carrotlovr_420"
+                        reviewMsg="The server ate part of my child like wtf"
+                    />
+                </div>
             </div>
         </div>
     )
